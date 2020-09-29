1b1c3df914184d95f9195b4482727dbbb770a276
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-09-28 17:02:11 -0700

    added the the user POST with working test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,25 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+// const UserService = require('../lib/services/user-service');
 
 describe('tardygram routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
   });
+
+  it('user signup via POST', async() => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'nate@nate.com',
+        password: 'password'
+      });
+
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      email: 'nate@nate.com'
+    });
+  });
+
 });
